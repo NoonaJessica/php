b37b0ffc822cc43870a25901d496649130313ab6
Noona <noonaposti@gmail.com>
2024-03-20 13:07:22 +0200

    pohja

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Test</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <header>
+        <img src="https://placehold.it/200x100" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#login">login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
